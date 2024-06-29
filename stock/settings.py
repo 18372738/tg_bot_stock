ffa6de5264926f2107f1c72485f87151553d0775
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-06-30 02:04:41 +0300

    Add time reminder

--- stock/settings.py
+++ stock/settings.py
@@ -101,15 +101,15 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = EMAIL_HOST
 EMAIL_HOST_USER = EMAIL_HOST_USER
 EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
-EMAIL_PORT = '465'
-EMAIL_USE_SSL = True
-SERVER_EMAIL = EMAIL_HOST_USER
-DEFAULT_EMAIL = EMAIL_HOST_USER
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_SERVER = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_ADMIN = EMAIL_HOST_USER
 
 Q_CLUSTER = {
     "name": "stock",
-    "orm": "default",  # Use Django's ORM + database for broker
+    "orm": "default",
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- stock_models/send_emails.py
+++ stock_models/send_emails.py
@@ -1,21 +1,94 @@
-from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
-from stock_models.models import Client, Box
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock.settings')
 
+import smtplib
+from django.core.mail import send_mail
+from django.conf import settings
+from datetime import datetime
+from datetime import timedelta
+from models import Box
 
 def reminder():
-    boxes_in_storage = Box.objects.filter(status='in_storage')
+    time_now = datetime.date.today()
+    three_days_before_end = time_now + timedelta(days=3)
+    two_weeks_before_end = time_now + timedelta(weeks=2)
+    three_weeks_before_end = time_now + timedelta(weeks=3)
+    one_month_before_end = time_now + timedelta(days=30)
+    dates = [three_days_before_end,
+             two_weeks_before_end,
+             three_weeks_before_end,
+             one_month_before_end
+             ]
+    boxes_in_storage = Box.objects.filter(end_storage__in=dates)
     for box in boxes_in_storage:
+        try:
+            send_mail(
+                # title:
+                'напоминание',
+                # message:
+                f'конец срока хранения {box.end_storage}. /'
+                f'Вы можете забрать вещи сами или заказать доставку',
+                # from:
+                settings.EMAIL_HOST_USER,
+                # to:
+                [box.client.email],
+                fail_silently=False,
+            )
+        except smtplib.SMTPRecipientsRefused as e:
+            print(f'Error sending email:  {e}')
 
-        msg = EmailMultiAlternatives(
-            # title:
-            'напоминание',
-            # message:
-            f'конец срока хранения {box.end_storage}',
-            # from:
-            settings.EMAIL_HOST_USER,
-            # to:
-            [box.client.email]
-        )
-        msg.send()
-        print(f'сообщение отправлено на {box.client.email}')
+    pick_up_today = Box.objects.filter(end_storage=time_now)
+    for box in pick_up_today:
+        for box in boxes_in_storage:
+            try:
+                send_mail(
+                    # title:
+                    'напоминание',
+                    # message:
+                    f'Заберите вещи сегодня. Вы можете забрать вещи сами или заказать доставку. /'
+                    f'Если вы не заберете вещи, они будут храниться 6 месяцев,но/'
+                    f'тариф будет чуть выше. После чего вы их потеряете',
+                    # from:
+                    settings.EMAIL_HOST_USER,
+                    # to:
+                    [box.client.email],
+                    fail_silently=False,
+                )
+            except smtplib.SMTPRecipientsRefused as e:
+                print(f'Error sending email:  {e}')
+
+    one_month_before_today = time_now - timedelta(days=30)
+    two_month_before_today = time_now - timedelta(days=60)
+    three_month_before_today = time_now - timedelta(days=90)
+    four_month_before_today = time_now - timedelta(days=120)
+    five_month_before_today = time_now - timedelta(days=150)
+    six_month_before_today = time_now - timedelta(days=180)
+    dates = [one_month_before_today,
+             two_month_before_today,
+             three_month_before_today,
+             four_month_before_today,
+             five_month_before_today,
+             six_month_before_today
+             ]
+    expired_boxes = Box.objects.filter(end_storage__in=dates)
+    for box in expired_boxes:
+        pick_up_time = box.end_storage + timedelta(days=180)
+        try:
+            send_mail(
+                # title:
+                'напоминание',
+                # message:
+                f'Хранение просрочено. Заберите вещи до {pick_up_time}. /'
+                f'Вы можете забрать вещи сами или заказать доставку',
+                # from:
+                settings.EMAIL_HOST_USER,
+                # to:
+                [box.client.email],
+                fail_silently=False,
+            )
+        except smtplib.SMTPRecipientsRefused as e:
+            print(f'Error sending email:  {e}')
+
+
+if __name__ == '__main__':
+    reminder()
