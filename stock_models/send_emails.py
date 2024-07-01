3a87710b35defc25772480e4b8a98ad69fb6a7f6
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-07-01 23:17:29 +0300

    Update send_emails.py

--- stock_models/send_emails.py
+++ stock_models/send_emails.py
@@ -3,32 +3,38 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock.settings')
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 import datetime
+from django.utils import timezone
 import smtplib
 from django.core.mail import send_mail
 from django.conf import settings
 from datetime import timedelta
 from stock_models.models import Box
+from django.db.models import Q
 
 
 def reminder():
-    time_now = datetime.date.today()
-    three_days_before_end = time_now + timedelta(days=3)
-    two_weeks_before_end = time_now + timedelta(weeks=2)
-    three_weeks_before_end = time_now + timedelta(weeks=3)
-    one_month_before_end = time_now + timedelta(days=30)
-    dates = [three_days_before_end,
-             two_weeks_before_end,
-             three_weeks_before_end,
-             one_month_before_end
-             ]
-    boxes_in_storage = Box.objects.filter(end_storage__in=dates)
+    time_now = timezone.now()
+    before_2_days = time_now + timedelta(days=2)
+    before_4_days = time_now + timedelta(days=4)
+    before_13_days = time_now + timedelta(days=13)
+    before_15_days = time_now + timedelta(days=15)
+    before_20_days = time_now + timedelta(days=20)
+    before_22_days = time_now + timedelta(days=22)
+    before_29_days = time_now + timedelta(days=29)
+    before_31_days = time_now + timedelta(days=31)
+
+    boxes_in_storage = Box.objects.filter(status='in_storage').filter(Q(end_storage__gt=before_4_days) & Q(end_storage__lt=before_2_days) |
+        Q(end_storage__gt=before_15_days) & Q(end_storage__lt=before_13_days) |
+        Q(end_storage__gt=before_22_days) & Q(end_storage__lt=before_20_days) |
+        Q(end_storage__gt=before_31_days) & Q(end_storage__lt=before_29_days)
+    )
     for box in boxes_in_storage:
         try:
             send_mail(
                 # title:
                 'напоминание',
                 # message:
-                f'конец срока хранения {box.end_storage}. \n'
+                f'конец срока хранения {box.end_storage.date()}.\n'
                 f'Вы можете забрать вещи сами или заказать доставку.',
                 # from:
                 settings.EMAIL_HOST_USER,
@@ -39,7 +45,10 @@ def reminder():
         except smtplib.SMTPRecipientsRefused as e:
             print(f'Error sending email:  {e}')
 
-    pick_up_today = Box.objects.filter(end_storage=time_now)
+    before_1_day = time_now + timedelta(days=2)
+    after_1_day = time_now + timedelta(days=4)
+
+    pick_up_today = Box.objects.filter(status='in_storage').filter(Q(end_storage__gt=before_1_day) & Q(end_storage__lt=after_1_day))
     for box in pick_up_today:
         try:
             send_mail(
@@ -58,20 +67,27 @@ def reminder():
         except smtplib.SMTPRecipientsRefused as e:
             print(f'Error sending email:  {e}')
 
-    one_month_before_today = time_now - timedelta(days=30)
-    two_month_before_today = time_now - timedelta(days=60)
-    three_month_before_today = time_now - timedelta(days=90)
-    four_month_before_today = time_now - timedelta(days=120)
-    five_month_before_today = time_now - timedelta(days=150)
-    six_month_before_today = time_now - timedelta(days=180)
-    dates = [one_month_before_today,
-             two_month_before_today,
-             three_month_before_today,
-             four_month_before_today,
-             five_month_before_today,
-             six_month_before_today
-             ]
-    expired_boxes = Box.objects.filter(end_storage__in=dates)
+    after_29_days = time_now - timedelta(days=29)
+    after_31_days = time_now - timedelta(days=31)
+    after_59_days = time_now - timedelta(days=59)
+    after_61_days = time_now - timedelta(days=61)
+    after_89_days = time_now - timedelta(days=89)
+    after_91_days = time_now - timedelta(days=91)
+    after_119_days = time_now - timedelta(days=119)
+    after_121_days = time_now - timedelta(days=121)
+    after_149_days = time_now - timedelta(days=149)
+    after_151_days = time_now - timedelta(days=151)
+    after_179_days = time_now - timedelta(days=179)
+    after_181_days = time_now - timedelta(days=181)
+
+    expired_boxes = Box.objects.filter(status='expired').filter(
+        Q(end_storage__gt=after_31_days) & Q(end_storage__lt=after_29_days) |
+        Q(end_storage__gt=after_61_days) & Q(end_storage__lt=after_59_days) |
+        Q(end_storage__gt=after_91_days) & Q(end_storage__lt=after_89_days) |
+        Q(end_storage__gt=after_121_days) & Q(end_storage__lt=after_119_days) |
+        Q(end_storage__gt=after_151_days) & Q(end_storage__lt=after_149_days) |
+        Q(end_storage__gt=after_181_days) & Q(end_storage__lt=after_179_days)
+    )
     for box in expired_boxes:
         pick_up_time = box.end_storage + timedelta(days=180)
         try:
@@ -79,7 +95,7 @@ def reminder():
                 # title:
                 'напоминание',
                 # message:
-                f'Хранение просрочено. Заберите вещи до {pick_up_time}.\n'
+                f'Хранение просрочено. Заберите вещи до {pick_up_time.date()}.\n'
                 f'Вы можете забрать вещи сами или заказать доставку',
                 # from:
                 settings.EMAIL_HOST_USER,
@@ -89,6 +105,10 @@ def reminder():
             )
         except smtplib.SMTPRecipientsRefused as e:
             print(f'Error sending email:  {e}')
+
+
+def test_task():
+    print('success')
 
 
 if __name__ == '__main__':
